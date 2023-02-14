0a726303cc140d45ad1447c8abf934a5ffeaa0a3
Николай <Nikolay@MacBook-Pro-Nikolaj.local>
2023-02-14 09:16:15 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,4 +4,8 @@ const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
+
+  
 program.parse();
