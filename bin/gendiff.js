543b2ca2a040e4c5fa11b731605a39ebe72eaa3c
Николай <Nikolay@MacBook-Pro-Nikolaj.local>
2023-02-15 23:24:19 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
-import { Command } from 'commander/esm.mjs';
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
 const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
-  
 program.parse();
